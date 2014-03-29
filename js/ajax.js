27c39bd7a2eb7a624f9086dfbd67ee6643931e54
Ryan Brett <ryan@ryanbrett.net>
2014-03-29 10:44:39 -0500

    AJAX - edits start of play

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('#tns li').on('click', function(e) {
+		e.preventDefault();
+	
+	$.ajax({
+		url: 'php/ajax-image.php',
+		type: 'post',
+		data: {'page': $(this).attr('href')},
+		cache: false,
+		success: function(json) {
+			$.each(json, function(i, item) {
+				if(typeof item == 'object') {
+				newhtml += '
+				}
+			}
+		}
+	});
+});
+	
+}
